6652c2bb385fb1fa2f005d63d264cdedc120f71a
Zeden19 <61285631+Zeden19@users.noreply.github.com>
2021-05-27 21:12:59 -0400

    Error checking, smaller features

--- src/main/java/com/example/ControllerCalendar.java
+++ src/main/java/com/example/ControllerCalendar.java
@@ -25,8 +25,6 @@ import static com.example.addAssignmentController.deleteYear;
 public class ControllerCalendar {
 
     public static Text welcomeText;
-    public Text ChangeableNameDisplay;
-
 
     public static int[] dateScore = new int[31];
 
@@ -53,7 +51,6 @@ public class ControllerCalendar {
      */
     public void setText(String username) throws IOException {
         welcomeText.setText(username);
-
     }
 
     /*
@@ -151,11 +148,6 @@ public class ControllerCalendar {
      Post: Adds any assignments to the calendar
      */
     public void addAssignmentToCalendar() throws IOException, GeneralSecurityException {
-
-        // THIS IS WHAT CHANGES THE NAME FOR THE PERSON THAT LOGGED IN
-        ChangeableNameDisplay.setText(LoginController.nameAccount);
-
-
         updateDateScore();
         String[][] assignmentInfo = SheetsAPI.PullAssignments();
 
